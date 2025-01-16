efde7b80dc0521e50f6e0e6551a414e4c9c06fa8
public <lg969787839@gmail.com>
2025-01-16 08:35:21 +0800

    commit 1

--- src/main/java/com/cangzhousc/mydemo1/model/User.java
+++ src/main/java/com/cangzhousc/mydemo1/model/User.java
@@ -1,8 +1,23 @@
 package com.cangzhousc.mydemo1.model;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 public class User {
     private int id;
     private String name;
+    private Timestamp birthday;
+    private BigDecimal amount;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer age;
 
     public int getId() {
         return id;
@@ -19,4 +34,13 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setBirthday(Timestamp birthday) {this.birthday = birthday; };
+
+    public Timestamp getBirthday() {return birthday;};
+
+    public void setAmount(BigDecimal amount) { this.amount = amount; };
+
+    public BigDecimal getAmount() {return amount;};
+
 }
--- src/test/java/com/cangzhousc/mydemo1/Mydemo1ApplicationTests.java
+++ src/test/java/com/cangzhousc/mydemo1/Mydemo1ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.cangzhousc.mydemo1;
 
+import com.cangzhousc.mydemo1.mapper.UserMapper;
+import com.cangzhousc.mydemo1.model.User;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Resource
 @SpringBootTest
 class Mydemo1ApplicationTests {
-
+    @Resource
+    UserMapper userMapper;
     @Test
     void contextLoads() {
     }
+    @Test
+    void mydemo1() throws ParseException {
+        User user = new User();
+        user.setName("hello");
+        user.setAge(19);
+
+        String timeStr = "1990-08-01 00:01:02";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = sdf.parse(timeStr);
+        Timestamp timestamp = new Timestamp(date.getTime());
+        user.setBirthday(timestamp);
+        user.setAmount(new BigDecimal("15.9"));
+        userMapper.insert(user);
+    }
+
 
 }
